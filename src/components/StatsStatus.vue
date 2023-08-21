8583e51fc94ae79b5490787f3152e462cb1fdde3
Hady41 <h.hawy@ob-soft.com>
2023-08-21 13:44:02 +0300

    Adding generic list view + StatsStatus component

---
+++ src/components/StatsStatus.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row class="box mb-8">
+    <v-card elevation="4" shaped class="pa-4" style="display: flex; flex-wrap: wrap;width: 100%;">
+      <v-row :cols="12 / statuses.length" v-for="status in statuses" :key="status.id" style="margin-top: 0px">
+        <v-col cols="3">
+          <v-img :src="status.icon"></v-img>
+        </v-col>
+        <v-col cols="9">
+          <div>{{ itemCount[status.name] }}</div>
+          <div>{{ status.label }}</div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: ['itemCount','statuses'],
+  // data() {
+  //   return {
+  //   }
+  // },
+  methods: {
+
+    getStatus(status_id) {
+      return this.statuses.find(status => status.id === status_id)
+    },
+  }
+}
+</script>
