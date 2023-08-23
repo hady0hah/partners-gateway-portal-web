2ea17322e4b4080d8729130eac8a247d3d18a379
Hady41 <h.hawy@ob-soft.com>
2023-08-23 15:15:01 +0300

    T810_Forecasting_List_Page

--- src/plugins/endpoints.js
+++ src/plugins/endpoints.js
@@ -5,6 +5,10 @@ let endpoints={
     'deal_list' : {
         'url' : 'private/deals/list'
     }
+    ,
+    'forecasting_list' : {
+        'url' : 'private/fr/list'
+    }
 }
 
 export default endpoints
