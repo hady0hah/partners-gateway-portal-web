a9d2284d5b1deb221de7bf7d810ac42d2d5b7835
NH <nadim@ob-soft.com>
2023-08-29 12:51:35 +0000

    Added component type

---
+++ src/components/FormCollection.vue
@@ -0,0 +1,11 @@
+<template>
+  <form-section :section="{'fields':fields}"></form-section>
+</template>
+<script>
+export default {
+  props:['fields'],
+  components : {
+    FormSection: () => import('./FormSection.vue')
+  },
+}
+</script>
