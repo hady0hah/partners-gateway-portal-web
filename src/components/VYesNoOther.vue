849ba7a172de7d7dd4ff27ee31ec71ea400fc853
NH <nadim@ob-soft.com>
2023-08-29 15:24:35 +0000

    Added yesnoother component

---
+++ src/components/VYesNoOther.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p style="color: #205023">{{ field.label }}
+      <v-radio-group v-model="model" v-on:change="valueChange()" row>
+        <v-radio label="Yes" :value="true"></v-radio>
+        <v-radio label="No" :value="false"></v-radio>
+        <v-radio label="Other" :value="null"></v-radio>
+        <v-text-field v-if="model === null" v-model="model" label="Other"
+          outlined></v-text-field>
+      </v-radio-group>
+    </p>
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+      model: null,
+    }
+  },
+}
+</script>
