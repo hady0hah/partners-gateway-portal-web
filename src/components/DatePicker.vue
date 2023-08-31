e3803115ab3f5d1417f7a5a8535c377aacade246
NH <nadim@ob-soft.com>
2023-08-31 08:09:33 +0000

    Fixed events on datepicke, number, yesnoother

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,10 +1,10 @@
 <template>
     <v-menu :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="auto">
       <template v-slot:activator="{ on }">
-        <v-text-field v-model="model" append-icon="mdi-calendar" readonly v-bind="$attrs"
+        <v-text-field append-icon="mdi-calendar" v-model="model" readonly v-bind="$attrs"
           v-on="on"></v-text-field>
       </template>
-      <v-date-picker v-model="model" @input="menu2 = false"></v-date-picker>
+      <v-date-picker v-model="model" @input="menu2 = false" v-bind:value="$attrs.value" v-on:input="$emit('input', $event)"></v-date-picker>
     </v-menu>
 </template>
 <script>
@@ -15,5 +15,11 @@ export default {
       model: null,
     }
   },
+  methods : {
+    oni($event){
+      console.log($event)
+      this.$emit('input', $event)
+    }
+  }
 }
 </script>
--- src/components/VYesNoOther.vue
+++ src/components/VYesNoOther.vue
@@ -1,24 +1,20 @@
 <template>
-      <v-radio-group v-model="model" v-on:change="valueChange()" row  v-bind="$attrs">
+      <v-radio-group v-bind="$attrs" v-bind:value="$attrs.value" v-on:change="$emit('input', $event)" row>
         <v-radio label="Yes" :value="true"></v-radio>
         <v-radio label="No" :value="false"></v-radio>
         <v-radio label="Other" :value="null"></v-radio>
-        <v-text-field v-if="model === null" v-model="model" label="Other"
+        <v-text-field v-if="$attrs.value === null" v-bind:value="$attrs.value" v-on:input="$emit('input', $event)" label="Other"
           outlined></v-text-field>
       </v-radio-group>
 </template>
 <script>
-
-import FormMixin from "@/mixins/FormMixin";
-
-
 export default {
   props : ['field','form_name'],
-  mixins : [FormMixin],
   data() {
     return {
-      model: null,
     }
   },
+  methods : {
+  }
 }
 </script>
