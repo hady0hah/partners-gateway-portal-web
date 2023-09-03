0c632041020327959e2914bc324cb62a6ef949e4
NH <nadim@ob-soft.com>
2023-09-03 20:40:12 +0000

    Added add btn to FormCollection

--- src/components/FormCollection.vue
+++ src/components/FormCollection.vue
@@ -1,11 +1,34 @@
 <template>
-  <form-section :section="{ 'fields': fields }" v-bind:value="$attrs.value" v-on:input="$emit('input', $event)"></form-section>
+  <v-row>
+    <v-col v-for="item,index in items" :key="index" class="col-12">
+      <slot name="collection-item"><form-section :section="{ 'fields': fields }" v-bind:value="item" v-on:input="onInput($event, index)"></form-section></slot>
+    </v-col>
+    <v-col class="col-12">
+      <slot name="collection-btn-add"><button type="button" style="text-align: center; background-color: #E0E0E0;font-family: Helvetica;border: none;border-radius: 5px;font-size: 10px;font-weight: bold;color: black;padding: 5px 10px 5px 10px;" @click="addItem">ADD MORE</button></slot>
+    </v-col>
+  </v-row>
+  
+  
 </template>
 <script>
 export default {
   props: ['fields'],
+  data() {
+    return {
+      items: this.$attrs.value?this.$attrs.value:[]
+    }
+  },
   components: {
     FormSection: () => import('./FormSection.vue')
   },
+  methods: {
+    addItem() {
+      this.items.push({});
+    },
+    onInput($event, index) {
+      this.items[index] = $event
+      this.$emit('input', this.items)
+    }
+  }
 }
 </script>
