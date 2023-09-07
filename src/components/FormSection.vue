a466b12fb2073ac71949a47843f9da58b1e10e1c
NH <nadim@ob-soft.com>
2023-09-07 17:16:01 +0000

    Fixed QBR form

--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -9,8 +9,8 @@
       </div>
     </v-col>
     <slot name="section-fields" v-bind:section="section">
-    <v-col v-for="field, k in section.fields" :key="k" :class="'col-12 col-md-4'">
-        <form-field :field="field" :form_name="form_name" v-bind:value="getFieldValue(field)" v-on:input="onInput(field,$event)"></form-field>
+    <v-col v-for="field, k in section.fields" :key="k" :class="getRowClass(field)">
+      <form-field :field="field" :form_name="form_name" v-bind:value="getFieldValue(field)" v-on:input="onInput(field,$event)"></form-field>
     </v-col>
     </slot>
   </v-row>
@@ -20,7 +20,7 @@ import FormField from './FormField.vue';
 import SectionMixin from "@/mixins/SectionMixin";
 
 export default {
-  props : ['section','form_name'],
+  props : ['section','form_name','row_class'],
   components : {
     FormField
   },
@@ -30,17 +30,11 @@ export default {
     }
   },
   methods: {
-    // getFieldValue(field) {
-    //   if(!this.$attrs.value)
-    //     this.$attrs.value = {}
-    //   if(!(field.name in this.$attrs.value))
-    //     this.$attrs.value[field.name] = null
-    //   return this.$attrs.value[field.name]
-    // },
-    // onInput(field, $event) {
-    //   this.$attrs.value[field.name] = $event
-    //   this.$emit('input', this.$attrs.value)
-    // }
+    //TODO: HOT FIX:  rethink how to implement custom class for specific fields
+    getRowClass(field) {
+      return field.type == "Sonata\\AdminBundle\\Form\\Type\\CollectionType"?'col-12':'col-12 col-md-4'
+    }
+    // END OF HOT FIX
   },
 }
 </script>
