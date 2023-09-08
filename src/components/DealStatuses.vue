2a13699b07fca4a4da2068e59faa206db8769b08
NH <nadim@ob-soft.com>
2023-09-08 11:40:27 +0000

    Fixed deal value propagation

--- src/components/DealStatuses.vue
+++ src/components/DealStatuses.vue
@@ -1,33 +1,26 @@
 <template>
-    <v-select
-      v-model="model"
-      v-on:change="valueChange()"
+    <v-select      
       item-text="label"
       v-bind="$attrs"
+      v-on:input="$emit('input',$event)"
     >
       <template v-slot:item="{ item }">
-        <template
-          v-bind="$attrs"
-          v-on:change="valueChange()">
           <v-list-item-avatar>
             <img :src="item.icon"  />
           </v-list-item-avatar>
           <v-list-item-content>
             <v-list-item-title>{{ item.label }}</v-list-item-title>
           </v-list-item-content>
-        </template>
       </template>
     </v-select>
 </template>
 <script>
 
 import ComponentMapper from "@/components/ComponentMapper";
-import FormMixin from "@/mixins/FormMixin";
-
 
 export default {
   props : ['field','form_name'],
-  mixins : [FormMixin],
+
   methods: {
     getFieldComponent() {
       return ComponentMapper.mapType(this.field, this.name)
