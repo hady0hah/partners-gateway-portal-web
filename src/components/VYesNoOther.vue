2ac7cb3414a08a5d8684e0f983d3a147dece3821
NH <nadim@ob-soft.com>
2023-09-08 18:01:15 +0000

    Fixed yenoother

--- src/components/VYesNoOther.vue
+++ src/components/VYesNoOther.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-radio-group style="margin-top:-4px" v-bind="$attrs" v-bind:value="$attrs.value" v-model="model" v-on:input="$emit('input', $event)">
+  <v-radio-group style="margin-top:-4px" v-bind="$attrs" v-model="radioState" v-on:change="onInput">
       <!-- <p style="color: #205023;display: none;">{{$attrs.label}}</p> -->
       <v-row>
         <v-col><v-radio label="Yes" :value="true"></v-radio></v-col>
@@ -7,20 +7,39 @@
         <v-col><v-radio label="Other" :value="null"></v-radio></v-col>
         <v-col></v-col>
         <v-col class="col-12">
-          <v-text-field :disabled="model === true || model === false" label="Other" outlined v-bind:value="$attrs.value" v-on:input="$emit('input', $event)"></v-text-field>
+          <v-text-field :disabled="radioState === true || radioState === false" label="Other" outlined v-model="otherState" v-on:input="onInput"></v-text-field>
         </v-col>
       </v-row>
   </v-radio-group>
 </template>
 <script>
+
 export default {
   props : ['field','form_name'],
   data() {
     return {
-      model:null
+      radioState:null,
+      otherState:null,
     }
   },
+  created() {
+    this.getState()
+  },
   methods : {
+    onInput($event) {
+      this.$attrs.value = $event
+      this.getState()
+      this.$emit('input', $event)
+    },
+    getState() {
+      if(this.$attrs.value == true || this.$attrs.value == false) {
+        this.radioState = this.$attrs.value
+        this.otherState = null
+      } else {
+        this.radioState = null
+        this.otherState = this.$attrs.value
+      }
+    }
   }
 }
 </script>
